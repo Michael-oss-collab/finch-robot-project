2f4ce72ffb043f802cee5da154ae87e9aedf70fe
Michael-oss-collab <mkovler0001@mymail.lausd.net>
2026-05-28 10:40:50 -0700

    donefortoday

--- src/main/java/com/periodTwo/TesterFinch.java
+++ src/main/java/com/periodTwo/TesterFinch.java
@@ -6,9 +6,28 @@ public static void main(String[] args){
 
     FinalFinch bird = new FinalFinch ("A");
 
-    bird.setMove( "F", 10, 100 );
-
-
+    bird.setMove( "F", 60, 100 );
+bird.setTurn("L", 90, 50);
+bird.setMove("F", 22, 100);
+bird.setTurn("L", 90, 50);
+bird.setMove("F", 60, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 22, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 60, 100);
+bird.setTurn("L", 90, 50);
+bird.setMove("F", 22, 100);
+bird.setTurn ("L", 90, 50);
+bird.setMove("F", 60, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 22, 50);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 90, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 120, 100);
+bird.setTurn("L", 90, 50);
+bird.setMove("F", 20, 100);
+bird.setTurn("L", 90, 50);
 }
 
 }
