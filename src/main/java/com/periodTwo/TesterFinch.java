bfe30e518ddff41ad5d916907f24ae973b2ca4f4
Michael-oss-collab <mkovler0001@mymail.lausd.net>
2026-06-01 09:41:43 -0700

    Update TesterFinch.java

--- src/main/java/com/periodTwo/TesterFinch.java
+++ src/main/java/com/periodTwo/TesterFinch.java
@@ -12,22 +12,26 @@ bird.setMove("F", 22, 100);
 bird.setTurn("L", 90, 50);
 bird.setMove("F", 60, 100);
 bird.setTurn("R", 90, 50);
-bird.setMove("F", 22, 100);
+bird.setMove("F", 25, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 60, 100);
 bird.setTurn("L", 90, 50);
-bird.setMove("F", 22, 100);
+bird.setMove("F", 23, 100);
 bird.setTurn ("L", 90, 50);
 bird.setMove("F", 60, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 22, 50);
 bird.setTurn("R", 90, 50);
+//checkpoint 1
 bird.setMove("F", 90, 100);
 bird.setTurn("R", 90, 50);
-bird.setMove("F", 120, 100);
+bird.setMove("F", 108, 100);
 bird.setTurn("L", 90, 50);
 bird.setMove("F", 20, 100);
 bird.setTurn("L", 90, 50);
+bird.setMove("F", 108, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 80, 100);
 }
 
 }
