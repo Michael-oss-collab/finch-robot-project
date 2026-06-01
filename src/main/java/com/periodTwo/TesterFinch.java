3242c3223ec0f1828344b5573c2dbee063788995
Michael-oss-collab <mkovler0001@mymail.lausd.net>
2026-06-01 10:37:58 -0700

    daf
    edf

--- src/main/java/com/periodTwo/TesterFinch.java
+++ src/main/java/com/periodTwo/TesterFinch.java
@@ -27,11 +27,28 @@ bird.setMove("F", 90, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 108, 100);
 bird.setTurn("L", 90, 50);
-bird.setMove("F", 20, 100);
+bird.setMove("F", 40, 100);
+//checkpoint 2
 bird.setTurn("L", 90, 50);
-bird.setMove("F", 108, 100);
+bird.setMove("F", 90, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 80, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 80, 100);
+bird.setTurn("R", 90, 50);
+//checkpoint 3
+bird.setMove("F", 60, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 40, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 30, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 20, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 20, 100);
+bird.setTurn("R", 90, 50);
+
+
 }
 
 }
