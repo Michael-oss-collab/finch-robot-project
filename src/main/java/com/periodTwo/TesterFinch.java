2d6e910e87ecdf481a0bcdad4f1d890d6915b4bb
Michael-oss-collab <mkovler0001@mymail.lausd.net>
2026-06-02 10:42:39 -0700

    ad
    sfdg

--- src/main/java/com/periodTwo/TesterFinch.java
+++ src/main/java/com/periodTwo/TesterFinch.java
@@ -6,30 +6,30 @@ public static void main(String[] args){
 
     FinalFinch bird = new FinalFinch ("A");
 
-    bird.setMove( "F", 60, 100 );
-bird.setTurn("L", 90, 50);
-bird.setMove("F", 22, 100);
-bird.setTurn("L", 90, 50);
-bird.setMove("F", 60, 100);
-bird.setTurn("R", 90, 50);
-bird.setMove("F", 25, 100);
-bird.setTurn("R", 90, 50);
-bird.setMove("F", 60, 100);
-bird.setTurn("L", 90, 50);
-bird.setMove("F", 23, 100);
-bird.setTurn ("L", 90, 50);
-bird.setMove("F", 60, 100);
-bird.setTurn("R", 90, 50);
-bird.setMove("F", 22, 50);
-bird.setTurn("R", 90, 50);
+//     bird.setMove( "F", 60, 100 );
+// bird.setTurn("L", 90, 50);
+// bird.setMove("F", 22, 100);
+// bird.setTurn("L", 90, 50);
+// bird.setMove("F", 60, 100);
+// bird.setTurn("R", 90, 50);
+// bird.setMove("F", 25, 100);
+// bird.setTurn("R", 90, 50);
+// bird.setMove("F", 60, 100);
+// bird.setTurn("L", 90, 50);
+// bird.setMove("F", 23, 100);
+// bird.setTurn ("L", 90, 50);
+// bird.setMove("F", 60, 100);
+// bird.setTurn("R", 90, 50);
+// bird.setMove("F", 22, 50);
+// bird.setTurn("R", 90, 50);
 //checkpoint 1
-bird.setMove("F", 90, 100);
-bird.setTurn("R", 90, 50);
-bird.setMove("F", 108, 100);
-bird.setTurn("L", 90, 50);
-bird.setMove("F", 40, 100);
+// bird.setMove("F", 81, 100);
+// bird.setTurn("R", 90, 50);
+// bird.setMove("F", 90, 100);
+// bird.setTurn("L", 90, 50);
+// bird.setMove("F", 34, 100);
+// bird.setTurn("L", 90, 50);
 //checkpoint 2
-bird.setTurn("L", 90, 50);
 bird.setMove("F", 90, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 80, 100);
@@ -46,8 +46,8 @@ bird.setTurn("R", 90, 50);
 bird.setMove("F", 20, 100);
 bird.setTurn("R", 90, 50);
 bird.setMove("F", 20, 100);
-bird.setTurn("R", 90, 50);
-
+bird.setTurn("L", 90, 50);
+//checkpoint 4
 
 }
 
